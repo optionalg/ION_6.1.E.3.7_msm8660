3be0e76b0f701fc54e79644a8b89df22d43838ec
Ganesh <ganesh@mint-o.(none)>
2013-01-11 23:42:00 +0600

    Add ROW & ZEN Schedulers, zRam/Cleancache/zCache/frontswap

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Tue Jan 1 23:58:28 BDT 2013"
+#define UTS_VERSION "#4 SMP PREEMPT Mon Jan 7 07:48:57 BDT 2013"
 #define LINUX_COMPILE_BY "GI"
-#define LINUX_COMPILE_HOST "Maya"
+#define LINUX_COMPILE_HOST "mint-o"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
